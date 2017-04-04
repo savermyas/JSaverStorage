af1667abcf9a3704988914f78b439c48e9bad970
Dmitry Myasnikov <saver_is_not@bk.ru>
2017-04-04 23:46:11 +0300

    Add agent application code

---
+++ agent-app/src/main/resources/server-tray.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#CLASSPATH=./lib/derbytools.jar:./lib/derby.jar:./bin:./stubs:./
+#export CLASSPATH
+#java -cp ./bin -Djava.server.hostname=localhost -Djava.security.policy=java1.policy -Djava.server.codebase=stubs vstu.edu.ru.agent.TrayRunner
+java -cp ./bin -Djava.rmi.server.hostname=localhost -Djava.security.policy=java1.policy -Djava.server.codebase=stubs vstu.edu.ru.agent.TrayRunner
